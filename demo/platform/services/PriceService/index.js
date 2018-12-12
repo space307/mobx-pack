3c76cd69240b4bef7781cd83cf2bfc682c0c860e
Sergey Kulakov <s.kulakov@space307.com>
2018-12-12 16:08:15 +0300

    FV-100 fireImmediately fixed

--- demo/platform/services/PriceService/index.js
+++ demo/platform/services/PriceService/index.js
@@ -1,11 +1,13 @@
 // @flow
 import { observable, action, reaction } from 'mobx';
 import { ServiceConnector } from 'sources.js';
-import { PRICE_SERVICE, ASSET_SERVICE } from 'demo/platform/constants/moduleNames.js';
+import {
+  PRICE_SERVICE,
+  ASSET_SERVICE,
+} from 'demo/platform/constants/moduleNames.js';
 import context from 'demo/platform/helper/context.js';
 import type { PriceServiceInterface } from 'demo/platform/services/PriceService/typing/interfaces.js';
 import type { AssetServiceInterface } from 'demo/platform/services/AssetService/typing/interfaces.js';
-
 
 function randNumber(min, max) {
   return Math.floor(Math.random() * (max - (min + 1))) + min;
@@ -14,23 +16,25 @@ function randNumber(min, max) {
 export class PriceService implements PriceServiceInterface {
   @observable bidPrice: ?number = null;
   @observable askPrice: ?number = null;
-  asset:? string = null;
+  asset: ?string = null;
 
   assetService: AssetServiceInterface;
 
-
-  init(){
-
-  }
+  init() {}
   onBind(assetService: AssetServiceInterface) {
     this.assetService = assetService;
 
     reaction(
-      () => (this.assetService.selectedAssetData),
+      () => this.assetService.selectedAssetData,
       () => {
-        this.asset = this.assetService.selectedAssetData && this.assetService.selectedAssetData.id;
+        this.asset =
+          this.assetService.selectedAssetData &&
+          this.assetService.selectedAssetData.id;
         this.generatePrice();
-      }, true,
+      },
+      {
+        fireImmediately: true,
+      },
     );
 
     setInterval(() => {
@@ -38,29 +42,23 @@ export class PriceService implements PriceServiceInterface {
     }, 1000);
   }
 
-
   @action generatePrice() {
     if (this.assetService.selectedAssetData) {
       const { minPrice, spread } = this.assetService.selectedAssetData;
       this.bidPrice = randNumber(minPrice - 10, minPrice + 10);
-      this.askPrice = randNumber(minPrice + spread + 10, minPrice + (spread - 10));
+      this.askPrice = randNumber(
+        minPrice + spread + 10,
+        minPrice + (spread - 10),
+      );
     }
   }
 }
 
-
-
-export default ServiceConnector(
-  new PriceService(),
-  {
-    binder: context.binder,
-    onStart: 'init',
-    config: {
-      bindAs: PRICE_SERVICE,
-      onBind: [
-        [ASSET_SERVICE, 'onBind'],
-      ],
-    },
-
+export default ServiceConnector(new PriceService(), {
+  binder: context.binder,
+  onStart: 'init',
+  config: {
+    bindAs: PRICE_SERVICE,
+    onBind: [[ASSET_SERVICE, 'onBind']],
   },
-);
+});
