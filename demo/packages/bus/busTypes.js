d6a4cb8b7bd3fda896dc431e213ad7f3357474c3
Alexander Smirnov <a.smirnov@space307.com>
2019-02-06 14:32:54 +0300

    package_architecture_busMD - bus

--- demo/packages/bus/busTypes.js
+++ demo/packages/bus/busTypes.js
@@ -1,26 +1,31 @@
 // @flow
 
-/*import type { AssistantEventTypes } from 'widgets/assistant/constants/eventTypes.js';
-import type { SideBarEventTypes } from 'core/serviceCreators/sideBarManagerServiceCreator/constants/eventTypes.js';
-import type { StatisticsEventTypes } from 'core/serviceCreators/statisticsServiceCreator/constants/eventTypes.js';
-import type { PlatformOutEventTypes } from 'binary/helper/platformMiddleware/OutType.js';*/
 
-// bus types
-export type BusAvailableEventsType = {};
+/* export default class ApiMD {
+  platformApi;
+  constructor(platformApi) {
+    this.platformApi = platformApi;
+  }
 
-export type BusEventType<PayloadType> = {
-  type: BusAvailableEventsType,
-  payload: PayloadType,
+  getBalance(cb, type) {
+    this.platformApi.getBalance(cb, type);
+  }
+
+  selectAsset(asset) {
+    this.platformApi.selectAsset(asset);
+  }
+
+  subsBidPrice(cb) {
+    this.platformApi.subsBidPrice(cb);
+  }
+
+  subsSelectedAsset(cb){
+    this.platformApi.subsSelectedAsset(cb);
+  }
+
+} */
+
+
+export const PLATFORM_EVENTS = {
+  CURRENT_ASSET: 'PLF_CURRENT_ASSET',
 };
-
-/*
-// common event names constants
-export { ASSISTANT_EVENT_TYPES } from 'widgets/assistant/constants/eventTypes.js';
-export { SIDE_BAR_EVENT_TYPES } from 'core/serviceCreators/sideBarManagerServiceCreator/constants/eventTypes.js';
-export { STATISTICS_EVENT_TYPES } from 'core/serviceCreators/statisticsServiceCreator/constants/eventTypes.js';
-export { PAIR_EVENT_NAMES, RATE_EVENT_NAMES } from 'binary/helper/platformMiddleware/OutType.js';
-
-// export types
-export type { CurrentPairPayloadType } from 'binary/helper/platformMiddleware/OutType.js';
-export type { CurrentRatePayloadType } from 'binary/helper/platformMiddleware/OutType.js';
-*/
