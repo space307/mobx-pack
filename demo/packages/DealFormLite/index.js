ebfec00219a8e259079f8a83f46f1f6d6d0440aa
Alexander Smirnov <a.smirnov@space307.com>
2019-02-07 19:36:01 +0300

    package_architecture_2_simpleInJs - finish 2

--- demo/packages/DealFormLite/index.js
+++ demo/packages/DealFormLite/index.js
@@ -1,20 +1,14 @@
-/* eslint-disable */
-import index from './content/DealFormLite.jsx';
+import React from 'react';
 import InApi from './api/in.js';
 import Store from './content/store.js';
 import DealFormLite from './content/DealFormLite.jsx';
 import OutApi from './api/out.js';
-import React from 'react';
-import ApiMD from './middleware/ApiMD.js';
 import busMD from './middleware/BusMD.js';
 
 
+export { busMD };
 
-export { ApiMD, busMD };
-
-export default function(Middleware, bus, id) {
-
-
+export default function (Middleware, bus, id) {
   const context = {
     outApi: new OutApi(),
     inApi: new InApi(),
@@ -30,7 +24,7 @@ export default function(Middleware, bus, id) {
   context.middleware.start(context);
 
 
-  const component = React.createElement(DealFormLite, {store: context.store});
+  const component = React.createElement(DealFormLite, { store: context.store });
 
   return { component };
-};
+}
