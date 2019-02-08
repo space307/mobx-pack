9be14b161bc2abd33607b831e53364a24f35f328
Alexander Smirnov <a.smirnov@space307.com>
2019-02-08 20:16:58 +0300

    package_architecture_apiBus - remove useless files

--- demo/packages/DealFormLite/index.js
+++ demo/packages/DealFormLite/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import Store from './content/store.js';
 import DealFormLite from './content/DealFormLite.jsx';
-import busMD from './gateway/BusMD.js';
 
-
-export { busMD };
 
 export default function (bus, id) {
   const store = new Store({ bus, id });
