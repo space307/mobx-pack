b6dd859eb12b4358dcc0639b45e2921e38fcef9f
Alexander Smirnov <a.smirnov@space307.com>
2019-03-13 19:44:06 +0300

    opt - start new test

---
+++ src/lib/__test__/serviceUtils.spec.js
@@ -0,0 +1,28 @@
+import type {ServiceClassType} from "../typing/common";
+
+
+describe('serviceUtils test', () => {
+
+//createService(Service: ServiceClassType, protoAttrs?: ?Array<*>)
+
+
+  it('createService', () => {
+
+
+    class Test{
+      constructor(a, b){
+        ////this.a
+
+      }
+
+
+
+    }
+
+    //expect(1).toBe(1);
+
+
+  });
+
+
+});
